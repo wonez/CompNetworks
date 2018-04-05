93de2d0b59e2446fa964fa4b1198dff355ad98eb
wonez <ffazlic@yahoo.com>
2018-04-05 13:04:27 +0300

    konekcije

--- src/BattleGround.java
+++ src/BattleGround.java
@@ -2,120 +2,157 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-public class BattleGround extends JFrame implements ActionListener{
+public class BattleGround extends JFrame implements ActionListener {
 
     private InetAddress ip;
     private int port;
-    private DatagramSocket socket;
-
-    private int ships = 5;
+    private JButton[] fields;
+    private  static int counter = 5;
     private JLabel counterLabel;
-    private JButton fields[];
+    private JLabel textLabel;
     private JButton readyButton;
+    private byte[] sendBuffer;
+    private DatagramPacket packet;
+    private DatagramSocket socket;
+    private boolean opponentReady =false;
 
+    public BattleGround(InetAddress ip, int port ,DatagramSocket socket) throws HeadlessException {
 
-    public BattleGround(InetAddress ip, int port, DatagramSocket socket) throws HeadlessException {
         super("BattleShip");
-        setSize(500, 500);
+        setSize(500,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         this.ip = ip;
         this.port = port;
-        this.socket = socket;
+        this.socket=socket;
 
         fields = new JButton[20];
 
-        JPanel bricks = new JPanel(new GridLayout(1, 20));
-        for(int i=0; i<20;i ++){
-            JButton btn = new JButton(i + "");
-            btn.setFont(new Font("Arial", Font.PLAIN, 0));
-            btn.addActionListener(this);
+        JPanel bricks = new JPanel(new GridLayout(1,20));
+        for(int i = 0 ;i < 20 ; i++){
+            JButton btn = new JButton(i+"");
+            btn.setFont(new Font("Arial",Font.PLAIN,-1));
             btn.setBackground(Color.BLUE);
+            btn.addActionListener(this);
             btn.setOpaque(true);
             btn.setBorderPainted(false);
             fields[i] = btn;
             bricks.add(btn);
         }
-
-        JPanel panel = new JPanel(new GridLayout(13, 1));
-
-        for(int i=0; i<13; i++){
-            if(i == 6){
+        JPanel panel = new JPanel(new GridLayout(13,1));
+        for(int i = 0; i<13;i++){
+            if(i==6){
                 panel.add(bricks);
-            }else if(i == 2){
-                JPanel jp = new JPanel();
-                jp.add(new JLabel("Position your ships"), BorderLayout.CENTER);
-                panel.add(jp);
-            }else if(i == 3){
-                JPanel jp = new JPanel();
-                counterLabel = new JLabel("Ships: " + ships);
-                jp.add(counterLabel, BorderLayout.CENTER);
-                panel.add(jp);
-            }else if(i == 9){
-                JPanel jp = new JPanel();
-                readyButton = new JButton("I am ready");
-                readyButton.setVisible(false);
-                readyButton.addActionListener(this);
-                jp.add(readyButton, BorderLayout.CENTER);
-                panel.add(jp);
-            }else{
+            }
+            else if(i == 2){
+                textLabel = new JLabel("Position your ships");
+                JPanel textPanel = new JPanel();
+                textPanel.add(textLabel);
+                panel.add(textPanel);
+            }
+            else if(i == 3){
+                counterLabel = new JLabel("Ships : " + counter);
+                JPanel countPanel = new JPanel();
+                countPanel.add(counterLabel);
+                panel.add(countPanel);
+            }
+            else if(i==9){
+                 readyButton = new JButton("I am ready");
+                 JPanel buttonPanel = new JPanel();
+                 readyButton.setVisible(false);
+                 readyButton.addActionListener(this);
+                 buttonPanel.add(readyButton,BorderLayout.CENTER);
+                 panel.add(buttonPanel);
+            }
+            else{
                 panel.add(new JPanel());
+
             }
         }
-        add(panel, BorderLayout.CENTER);
+        this.add(panel,BorderLayout.CENTER);
 
-        PackageThread pThread = new PackageThread(this, socket) {
+        PackageThread pthread = new PackageThread(this,socket) {
             @Override
             public void handleConnection() throws Exception {
 
+
+                System.out.println("Primoo");
+                String msg = new String(receivedBuffer);
+                if(msg.contains("I am Ready")){
+                    textLabel.setText(Game.OPPONENT + " is ready");
+                    opponentReady = true;
+                }
+                if(msg.contains("I am also Ready")){
+                    System.out.println("otvaram novi game");
+                }
             }
         };
 
-        pThread.createAndStartListenThread();
-        pThread.createAndStartIsAliveThread();
-    }
+
+        pthread.createAndStartListenThread();
+        pthread.createAndStartIsAliveThread();
+        }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
         if(e.getActionCommand().equals("I am ready")){
-
             sendPacket();
-
-        }else if(ships > 0 && addShip(e.getActionCommand())) {
-            ships--;
-            counterLabel.setText("Ships " + ships);
         }
+        if(counter > 0 && addShip(e.getActionCommand())) {
+            counter--;
+            counterLabel.setText("Ships :" + counter);
 
-        if(ships == 0){
+        }
+       if(counter==0){
             readyButton.setVisible(true);
         }
     }
 
     private boolean addShip(String text){
-
-        int pos = 20;
+        int pos  = 20;
         try{
             pos = Integer.parseInt(text);
-        }catch (Exception e){
+
+        }
+        catch (Exception e){
             return false;
         }
 
-        if(pos +1 > 19 || fields[pos+1].getText().contains("S")) return false;
+        if(pos + 1 > 19 || fields[pos + 1].getText().contains("S"))return false;
 
         fields[pos].setBackground(Color.GRAY);
-        fields[pos + 1].setBackground(Color.GRAY);
-
+        fields[pos+1].setBackground(Color.GRAY);
         fields[pos].setText(pos + "S");
-        fields[pos + 1].setText((pos + 1)  + "S");
+        fields[pos+1].setText((pos+1) + "S");
 
         return true;
     }
 
     private void sendPacket(){
 
+        String msg = "I am Ready";
+
+        if(opponentReady){
+            msg = "I am also Ready";
+        }else{
+            textLabel.setText("Waiting on " + Game.OPPONENT);
+            readyButton.setEnabled(false);
+        }
+
+        sendBuffer = msg.getBytes();
+        packet = new DatagramPacket(sendBuffer, msg.length(), ip, port);
+
+        try {
+            socket.send(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if(opponentReady) System.out.println("Otvara mudo");
     }
 }
